5dddeb98d7d755419f5b97688e2e4c2140b4eb0b
AbnerDHJ <abnerdanielh@gmail.com>
2024-09-26 15:20:00 -0600

    modificacion main

---
+++ aplicacion_escuela/lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Text('Bienvenido a la App de Gestión para Maestros'),
+      ),
+    );
+  }
+}
